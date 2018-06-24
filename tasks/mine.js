c34f6ad49f85d888c2090af84fb68f55fc3f5425
Denis Smirnov <smdenis@yandex-team.ru>
2018-06-24 17:06:21 +0300

    wip

--- tasks/mine.js
+++ tasks/mine.js
@@ -7,15 +7,15 @@ const db = require('../lib/db');
 mine()
     .then(fixup) // Нормализация и добавление производных данных
     .then(dedup) // Удаляем дубли
-    .then(filters) // Оставляем подходящие по цене и другим параметрам
-    .then(offers => db.addOffers(offers))
-    .then(res => {
-        console.log('Объявления добавлены', res.length);
-        console.log(res.map(o => o.uniqueKey));
-        process.exit();
-    })
-    .catch(e => {
-        console.error('Что-то пошло не так');
-        console.error(e)
-        process.exit();
-    });
+    // .then(filters) // Оставляем подходящие по цене и другим параметрам
+    // .then(offers => db.addOffers(offers))
+    // .then(res => {
+        // console.log('Объявления добавлены', res.length);
+        // console.log(res.map(o => o.uniqueKey));
+        // process.exit();
+    // })
+    // .catch(e => {
+        // console.error('Что-то пошло не так');
+        // console.error(e)
+        // process.exit();
+    // });
