d09709b4ac2ef6043809c97b1932c422ca1b746d
Misha Berezin <mishaberezin@gmail.com>
2018-08-03 19:30:25 +0300

    Парсер для Недвижки

--- config/default.js
+++ config/default.js
@@ -2,5 +2,9 @@ module.exports = {
     cian: {
         username: 'ubiquitous-bassoon@ya.ru',
         password: 'dWJpcXVpdG91cy1iYXNzb29u'
+    },
+    realty: {
+        username: 'improved-octo-system@ya.ru',
+        password: 'aW1wcm92ZWQtb2N0by1zeXN0ZW0'
     }
 }
