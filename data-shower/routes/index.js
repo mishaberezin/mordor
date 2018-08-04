2cb07289f22eee6c96911f6d6fbcb38462ca2e26
Denis Smirnov <smdenis@yandex-team.ru>
2018-08-04 13:39:08 +0300

    cian: infinitely

--- data-shower/routes/index.js
+++ data-shower/routes/index.js
@@ -1,25 +1,56 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const fs = require('fs');
+const fetch = require('node-fetch');
 
-const base = require('../../lib/base');
-
-// let data = JSON.parse(fs.readFileSync('../data/data.json'));
-// // let data = JSON.parse(fs.readFileSync('../data/data-filtered.json'));
-// data.sort((a,b) => a.address.localeCompare(b.address));
-
-// data = Object.values(data.reduce((res, apt) => {
-    // let key = apt.address.replace(/\s/g, '-');
-
-    // res[key] = [...(res[key] || []), apt];
-
-    // return res;
-// }, {}));
+const base = require('../../lib/db');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    let data = await base.getOffers({});
-    res.render('index', { data });
+    let offers = await base.getOffers({});
+    res.render('index', { offers });
+});
+
+router.get('/:id', async function(req, res, next) {
+    const id = req.params.id;
+    let offer = (await base.getOffers({ _id: id}))[0];
+    let places = [].concat.apply([], (await base.getPlaces()).map(a => a.items));
+    let offerCoords = offer.address.meta.geometry.coordinates;
+
+    let closePlaces = places.map(p => {
+        const lat = p.coordinates[0];
+        const lang = p.coordinates[1];
+        const dLat = Math.abs(lat - offerCoords[0]);
+        const dLang = Math.abs(lang - offerCoords[1]);
+        const dCoord = dLat < 0.025 && dLang < 0.025;
+
+        if (!dCoord) return false;
+
+        return Object.assign(p, { d: dLat + dLang })
+    })
+        .filter(Boolean)
+
+    const distances = await Promise.all(closePlaces.map(p => (
+        new Promise(resolve => (
+            fetch(`http://localhost:8989/route?point=${offerCoords.join(',')}&point=${p.coordinates.join(',')}&instructions=false&vehicle=foot`)
+            .then(res => res.json())
+            .then(({paths}) => ({
+                distance: paths[0].distance,
+                distanceText: (paths[0].distance < 1000 ?
+                    Math.ceil(paths[0].distance.toFixed(0, 10) / 10) * 10 + 'м' :
+                    (paths[0].distance/1000).toFixed(1, 10) + 'км'),
+                time: (paths[0].distance / 80).toFixed(0, 10),
+                timeText: (paths[0].distance / 80).toFixed(0, 10) + ' мин.'
+            }))
+            .then(resolve)
+        ))
+    )));
+
+    closePlaces = closePlaces.map((p, i) => Object.assign(p, distances[i]))
+        .filter(a => a.time < 20)
+        .sort((a, b) => a.distance > b.distance ? 1 : -1)
+
+    res.render('offer', { offer, places: closePlaces });
 });
 
 module.exports = router;
---
+++ tasks/mine-shops.js
@@ -0,0 +1,15 @@
+const mine = require('../lib/mine-shops');
+const db = require('../lib/db');
+
+mine()
+    .then(async res => {
+        const addResult = await db.addPlaces(res);
+        console.log(addResult);
+        process.exit(0)
+        return addResult;
+    })
+    .catch(e => {
+        console.error('Что-то пошло не так');
+        console.error(e)
+        process.exit();
+    });
