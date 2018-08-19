7fb0ca588618767da76508cad53985dd55dcd851
Misha Berezin <mishaberezin@gmail.com>
2018-08-19 18:11:43 +0300

    fixes

--- config/default.js
+++ config/default.js
@@ -1,12 +1,10 @@
 module.exports = {
-    robots: {
-        cian: {
-            username: 'ubiquitous-bassoon@ya.ru',
-            password: 'dWJpcXVpdG91cy1iYXNzb29u'
-        },
-        realty: {
-            username: 'improved-octo-system@ya.ru',
-            password: 'aW1wcm92ZWQtb2N0by1zeXN0ZW0',
-        }
+    cian: {
+        username: 'ubiquitous-bassoon@ya.ru',
+        password: 'dWJpcXVpdG91cy1iYXNzb29u'
+    },
+    realty: {
+        username: 'improved-octo-system@ya.ru',
+        password: 'aW1wcm92ZWQtb2N0by1zeXN0ZW0',
     }
 }
