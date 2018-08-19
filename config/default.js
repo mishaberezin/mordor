342d4865aa725bcfb0d6b0d5a0059ec7e5fdb554
Misha Berezin <mishaberezin@gmail.com>
2018-08-19 15:23:04 +0300

    Парсер realty

--- config/default.js
+++ config/default.js
@@ -1,10 +1,12 @@
 module.exports = {
-    cian: {
-        username: 'ubiquitous-bassoon@ya.ru',
-        password: 'dWJpcXVpdG91cy1iYXNzb29u'
-    },
-    realty: {
-        username: 'improved-octo-system@ya.ru',
-        password: 'aW1wcm92ZWQtb2N0by1zeXN0ZW0'
+    robots: {
+        cian: {
+            username: 'ubiquitous-bassoon@ya.ru',
+            password: 'dWJpcXVpdG91cy1iYXNzb29u'
+        },
+        realty: {
+            username: 'improved-octo-system@ya.ru',
+            password: 'aW1wcm92ZWQtb2N0by1zeXN0ZW0',
+        }
     }
 }
