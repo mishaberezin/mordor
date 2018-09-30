ff4df5bbf4a356c94b882f329cf7799b98ef9baa
Misha Berezin <mishaberezin@gmail.com>
2018-09-30 15:38:03 +0300

    fix

--- server/offer.js
+++ server/offer.js
@@ -2,16 +2,11 @@ const db = require('../lib/db');
 
 const add = (req, res) => {
 
-    console.log('Пришла порция');
-    
     db.addRawOffers(req.body.offers)
         .then(() => {
             res.status(200).send('OK')
         })
         .catch(err => {
-            console.log('===========================');
-            console.log('500!!!!!!!!!!!!!');
-            console.log('===========================');
             res.status(500).send({ error: err.toString() });
         });
 }
