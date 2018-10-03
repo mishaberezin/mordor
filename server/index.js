871ead251ee22d01cf1d11d89d7d9f2e824f68f9
Misha Berezin <mishaberezin@gmail.com>
2018-10-03 21:29:17 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ Sentry.init({ dsn: 'https://a0c7bf7efcc64a918907855a88f501b1@sentry.io/1256061' 
 const offer = require('./offer');
 
 const PORT = 3000;
-const DIRECT_CALL = !module.parent;
 
 const app = express();
 
@@ -25,9 +24,5 @@ app.use((req, res, next) => {
     res.status(404).send("Fin!")
 });
 
-if(DIRECT_CALL) {
-    app.listen(PORT);
-    console.log('Express started on port 3000');
-} else {
-    module.exports = app;
-}
+app.listen(PORT);
+console.log('Express started on port 3000');
